a2cba088d906502ec51c28713872256a3e1e2659
Lolis-ui <heloisasantosdosgomes0102@gmail.com>
2026-02-11 20:52:46 -0300

    Aula01

---
+++ js-aula1-tipos-de-funcoes/variaveis/script.js
@@ -0,0 +1,22 @@
+// Ctrl e ; é um atalho para escrever comentários
+
+// VAR: No geral evite seu uso, pode não ser muito seguro. Sobreescreve valores armazenados nas variaveis
+
+var cidade = "Registro";
+var cidade = "Sete Barras";
+document.write(cidade);
+// 
+
+// LET: Utilize quando for necessário reatribuir o valor da variavel
+
+let nome= "Maria";
+nome = "Diego";
+document.write("<br>");
+document.write(nome);
+
+// CONST: Utilize quando NÂO precisar reatribuir valor da variavel
+
+const pais = "Brasil";
+const pais = "Argentina";
+document.write("<br>");
+document.write(pais);
